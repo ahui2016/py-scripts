112490c1c72b90cc61e058d927604b0a68e37d80
ahui <ahui.2015@icloud.com>
2022-11-08 18:06:46 +0800

    add `tempbk download`

--- src/scripts/cf_r2.py
+++ src/scripts/cf_r2.py
@@ -8,6 +8,7 @@ import boto3
 import msgpack
 import tomli_w
 import arrow
+from humanfriendly import format_size
 from botocore.exceptions import ClientError
 
 
@@ -20,18 +21,20 @@ https://developers.cloudflare.com/r2/examples/boto3/
 
 """
 【关于返回值】
-本项目的返回值经常采用 (result, err_code) 的形式,
-err_code 是 int, 小于零表示有错误, 大于等于零表示没有错误.
-当 err_code 小于零时, result 的类型是 str, 内容是关于错误的说明.
-当 err_code 大于等于零时, result 就是有效的数据.
+本项目的返回值有时采用 (result, err) 的形式,
+err 是 str, 有内容表示有错误, 空字符串表示没错误.
+
+【关于 object 与 file】
+为了便于区分, 在代码中将本地文件称为 file, 云端文件称为 object.
 """
 
-Files_Summary_Name = "files-summary.msgp"
+Objects_Summary_Name = "objects-summary.msgp"
 """
 dict[str, int]  # 日期(年月)与文件数量 '202201': 5
 """
 default_summary = {}
 
+Download_Dir = "download_dir"
 Boto3_Config_Filename = "boto3_config.toml"
 boto3_config_file = config.app_config_dir.joinpath(Boto3_Config_Filename)
 
@@ -46,8 +49,12 @@ def default_config():
 
 def ensure_config_file() -> None:
     if not boto3_config_file.exists():
-        with open(boto3_config_file, "wb") as f:
-            tomli_w.dump(default_config(), f)
+        write_boto3_cfg(default_config())
+
+
+def write_boto3_cfg(boto3_cfg):
+    with open(boto3_config_file, "wb") as f:
+        tomli_w.dump(boto3_cfg, f)
 
 
 def get_boto3_cfg():
@@ -76,47 +83,66 @@ def get_s3_client(boto3_cfg):
     )
 
 
-def get_files_summary(bucket):
+def set_download_dir(dir_path:str, boto3_cfg):
+    boto3_cfg[Download_Dir] = dir_path
+    write_boto3_cfg(boto3_cfg)
+
+
+def get_download_dir(boto3_cfg):
+    return Path(boto3_cfg[Download_Dir])
+
+
+def check_download_dir(boto3_cfg):
+    """
+    :return: err: str
+    """
+    if Download_Dir in boto3_cfg and len(boto3_cfg[Download_Dir]) > 0:
+        return ""
+    return "请先设置文件夹用于保存下载文件, 例如:\n" \
+        "tempbk download -dir /path/to/folder"
+
+
+def get_summary(bucket):
     try:
-        data = get_file_obj(Files_Summary_Name, bucket)
+        data = get_obj_data(Objects_Summary_Name, bucket)
         summary = msgpack.unpackb(data.getvalue())
     except ClientError as err:
         if err.__str__().lower().find("not found") < 0:
             raise
         # 云端找不到 summary 文件, 因此新建.
-        upload_files_summary(default_summary, bucket)
+        upload_summary(default_summary, bucket)
         summary = default_summary
     return summary
 
 
-def upload_files_summary(files_summary, bucket):
-    data = msgpack.packb(files_summary)
-    bucket.upload_fileobj(io.BytesIO(data), Files_Summary_Name)
+def upload_summary(summary, bucket):
+    data = msgpack.packb(summary)
+    bucket.upload_fileobj(io.BytesIO(data), Objects_Summary_Name)
 
 
-def get_file_obj(obj_name, bucket):
+def get_obj_data(obj_name, bucket):
     data = io.BytesIO()
     bucket.download_fileobj(obj_name, data)
     return data
 
 
-def update_files_summary(obj_name, files_summary):
+def update_summary(obj_name, summary):
     """上传文件时, 统计数字加一."""
     month = obj_name[:6]
-    n = files_summary.get(month, 0)
-    files_summary[month] = n + 1
-    return files_summary
+    n = summary.get(month, 0)
+    summary[month] = n + 1
+    return summary
 
 
-def minus_files_summary(deleted, files_summary):
+def minus_summary(deleted, summary):
     """删除文件时, 根据删除结果更新统计数字."""
     for obj in deleted:
         month = obj['Key'][:6]
-        n = files_summary.get(month, 0)
-        files_summary[month] = n - 1
-        if (n <= 0) and (month in files_summary):
-            del files_summary[month]
-    return files_summary
+        n = summary.get(month, 0)
+        summary[month] = n - 1
+        if (n <= 0) and (month in summary):
+            del summary[month]
+    return summary
 
 
 def print_deleted(deleted):
@@ -133,62 +159,95 @@ def add_prefix(filepath: Path):
     return f"{today()}/{filepath.name}"
 
 
-def upload_file(filepath, files_summary, bucket):
+def upload_file(filepath, summary, bucket):
     obj_name = add_prefix(filepath)
     exists = obj_exists(obj_name, bucket)
     filepath_str = str(filepath)
     bucket.upload_file(
-        filepath_str, obj_name, Callback=ProgressPercentage(filepath_str)
+        filepath_str, obj_name, Callback=UploadProgress(filepath_str)
     )
     if not exists:
-        summary = update_files_summary(obj_name, files_summary)
-        upload_files_summary(summary, bucket)
+        summary = update_summary(obj_name, summary)
+        upload_summary(summary, bucket)
 
 
 def obj_exists(obj_name, bucket):
-    files = get_file_list(obj_name, bucket)
-    for obj in files:
+    objects = get_objects_by_prefix(obj_name, bucket)
+    for obj in objects:
         if obj.key == obj_name:
             return True
     return False
 
 
-def get_file_list(prefix, bucket):
+def get_objects_by_prefix(prefix, bucket):
     if prefix == "today":
         prefix = today()
     return bucket.objects.filter(Prefix=prefix)
 
 
-def print_file_list(files):
+def objects_to_list(objects):
+    """objects 是迭代器, 有时需要转换为 list."""
+    obj_list = []
+    for obj in objects:
+        obj_list.append(dict(key=obj.key, size=obj.size))
+    return obj_list
+
+
+def objects_to_delete(objects):
+    """专门转换为待删除列表"""
+    obj_list = []
+    for f in objects:
+        obj_list.append(dict(Key=f.key))
+    return obj_list
+
+
+def print_objects_with_size(objects):
+    for obj in objects:
+        print(f"({format_size(obj['size'])}) {obj['key']}")
+
+
+def print_objects_key(objects):
     i = 0
-    for f in files:
-        print(f.key)
+    for obj in objects:
+        print(obj.key)
         i += 1
     return i
 
 
-def get_object_list(files):
-    objects = []
-    for f in files:
-        objects.append(dict(Key=f.key))
-    return objects
-
-
-def delete_objects(objects, files_summary, bucket):
+def delete_objects(objects, summary, bucket):
     resp = bucket.delete_objects(
         Delete={'Objects': objects}
     )
     deleted = resp['Deleted']
-    summary = minus_files_summary(deleted, files_summary)
-    upload_files_summary(summary, bucket)
+    summary = minus_summary(deleted, summary)
+    upload_summary(summary, bucket)
     if len(objects) != len(deleted):
         print_deleted(deleted)
     if 'Errors' in resp:
         print(resp['Errors'])
 
 
+def get_download_filepath(obj_name:str, folder:Path, filepath:Path):
+    """
+    :return: (result, err_code)
+    """
+    if filepath is None:
+        filepath = folder.joinpath(obj_name[9:])
+    if not filepath.parent.exists():
+        return None, f"文件夹不存在: {filepath.parent}"
+    if filepath.exists():
+        return filepath, f"文件已存在: {filepath}"
+    return filepath, ""
+
+
+def download_file(bucket, obj_name, size, filepath):
+    bucket.download_file(
+        obj_name, filepath, Callback=DownloadProgress(obj_name, size)
+    )
+
+
 # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
-class ProgressPercentage(object):
+class UploadProgress(object):
     def __init__(self, filename):
         self._filename = filename
         self._size = float(os.path.getsize(filename))
@@ -204,4 +263,33 @@ class ProgressPercentage(object):
                 "\r%s  %s / %s  (%.2f%%)" % (
                     self._filename, self._seen_so_far, self._size,
                     percentage))
+            sys.stdout.flush()
+
+
+# https://gist.github.com/egeulgen/538aadc90275d79d514a5bacc4d5694e
+class DownloadProgress(object):
+    def __init__(self, filename, size):
+        self._filename = filename
+        self._size = size
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+        self.prog_bar_len = 80
+
+    def __call__(self, bytes_amount):
+        # To simplify we'll assume this is hooked up to a single filename.
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            ratio = round((float(self._seen_so_far) / float(self._size)) * (self.prog_bar_len - 6), 1)
+            current_length = int(round(ratio))
+
+            percentage = round(100 * ratio / (self.prog_bar_len - 6), 1)
+
+            bars = '+' * current_length
+            output = bars + ' ' * (self.prog_bar_len - current_length - len(str(percentage)) - 1) + str(
+                percentage) + '%'
+
+            if self._seen_so_far != self._size:
+                sys.stdout.write(output + '\r')
+            else:
+                sys.stdout.write(output + '\n')
             sys.stdout.flush()
--- src/tempbk.py
+++ src/tempbk.py
@@ -15,7 +15,7 @@ boto3_cfg: dict
 s3: Any
 s3_client: Any
 the_bucket: Any
-files_summary: dict
+objects_summary: dict
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
@@ -29,12 +29,12 @@ def print_err(err):
 @click.pass_context
 def cli(ctx):
     """Temp Backup: 临时备份文件"""
-    global boto3_cfg, s3, s3_client, the_bucket, files_summary
+    global boto3_cfg, s3, s3_client, the_bucket, objects_summary
     boto3_cfg = cf_r2.get_boto3_cfg()
     s3 = cf_r2.get_s3(boto3_cfg)
     s3_client = cf_r2.get_s3_client(boto3_cfg)
     the_bucket = cf_r2.get_bucket(s3, boto3_cfg)
-    files_summary = cf_r2.get_files_summary(the_bucket)
+    objects_summary = cf_r2.get_summary(the_bucket)
     print()
 
 
@@ -44,7 +44,6 @@ def cli(ctx):
 
 
 @cli.command(context_settings=CONTEXT_SETTINGS)
-# @click.argument("file", nargs=1, type=click.Path(exists=True))
 @click.pass_context
 def info(ctx):
     """Show information."""
@@ -57,7 +56,7 @@ def info(ctx):
 @click.pass_context
 def count(ctx):
     """Count files uploaded."""
-    print(files_summary)
+    print(objects_summary)
     print()
 
 
@@ -66,7 +65,7 @@ def count(ctx):
 @click.pass_context
 def upload(ctx, file):
     """Upload a file."""
-    cf_r2.upload_file(Path(file), files_summary, the_bucket)
+    cf_r2.upload_file(Path(file), objects_summary, the_bucket)
     print()
 
 
@@ -74,9 +73,9 @@ def upload(ctx, file):
 @click.argument("prefix", nargs=1, type=str)
 @click.pass_context
 def list_command(ctx, prefix):
-    """List files by prefix.
+    """List objects by prefix.
 
-    通过前缀查找文件, 例如:
+    通过前缀查找云端文件, 例如:
 
     tempbk list 202211 (列出2022年11月的全部文件)
 
@@ -84,8 +83,8 @@ def list_command(ctx, prefix):
 
     tempbk list today  (列出今天的全部文件)
     """
-    files = cf_r2.get_file_list(prefix, the_bucket)
-    i = cf_r2.print_file_list(files)
+    objects = cf_r2.get_objects_by_prefix(prefix, the_bucket)
+    i = cf_r2.print_objects_key(objects)
     if i == 0:
         print(f"Not Found: {prefix}")
     print()
@@ -95,7 +94,7 @@ def list_command(ctx, prefix):
 @click.argument("prefix", nargs=1, type=str)
 @click.pass_context
 def delete(ctx, prefix):
-    """Delete files by prefix.
+    """Delete objects by prefix.
 
     删除指定前缀的云端文件(必须包含日期前缀), 例如:
 
@@ -103,18 +102,98 @@ def delete(ctx, prefix):
 
     tempbk delete 202211           (删除2022年11月的全部文件)
     """
-    files = cf_r2.get_file_list(prefix, the_bucket)
-    objects = cf_r2.get_object_list(files)
-    length = len(objects)
+    objects = cf_r2.get_objects_by_prefix(prefix, the_bucket)
+    obj_del_list = cf_r2.objects_to_delete(objects)
+    length = len(obj_del_list)
     if length == 0:
         print(f"Not Found: {prefix}")
         print("(注意, 必须以日期前缀开头, 并且文件名区分大小写)")
         ctx.exit()
 
-    cf_r2.print_file_list(files)
-    click.confirm(f"\nDelete {length} files? (确认删除云端文件)", abort=True)
-    cf_r2.delete_objects(objects, files_summary, the_bucket)
+    cf_r2.print_objects_key(objects)
+    click.confirm(f"\nDelete {length} objects? (确认删除云端文件)", abort=True)
+    cf_r2.delete_objects(obj_del_list, objects_summary, the_bucket)
     print()
+
+
+@cli.command(context_settings=CONTEXT_SETTINGS)
+@click.option(
+    "folder",
+    "-dir",
+    help="Specify a folder to save the downloaded file.",
+)
+@click.option(
+    "dest",
+    "--save-to",
+    help="Specify a path to save the downloaded file.",
+)
+@click.argument("prefix")
+@click.pass_context
+def download(ctx, folder, dest, prefix):
+    """Download a file.
+
+    下载前可先指定保存文件的文件夹, 例如:
+
+    tempbk download -dir /path/to/folder
+
+    只需要设置一次, 后续下载就会一律保存在指定的文件夹,
+    如果需要更换文件夹可重新设置.
+
+    下载指定前缀的云端文件(必须包含日期前缀), 例如:
+
+    tempbk download 20221111/abc.txt (下载2022年11月11日名为abc.txt的文件)
+
+    第二章下载方法, 不管文件夹, 直接指定下载到哪个位置, 例如:
+
+    tempbk download 20221111/abc.txt --save-as /path/to/cde.txt
+    """
+    if err := cf_r2.check_download_dir(boto3_cfg):
+        if not folder:
+            print_err(err)
+            ctx.exit()
+
+    if (not folder) and (not dest) and (not prefix):
+        print(
+            "下载指定前缀的云端文件(必须包含日期前缀), 例如:\n"
+            "tempbk download 20221111/abc.txt"
+        )
+        ctx.exit()
+
+    if folder:
+        folder = Path(folder).resolve()
+        cf_r2.set_download_dir(str(folder), boto3_cfg)
+    else:
+        folder = cf_r2.get_download_dir(boto3_cfg)
+
+    if not dest:
+        dest = None
+
+    if prefix:
+        objects = cf_r2.get_objects_by_prefix(prefix, the_bucket)
+        obj_list = cf_r2.objects_to_list(objects)
+        length = len(obj_list)
+        if length == 0:
+            print(f"Not Found: {prefix}")
+            print("(注意, 必须以日期前缀开头, 并且文件名区分大小写)")
+            ctx.exit()
+        if length > 1:
+            print("每次只能下载一个文件:\n")
+            cf_r2.print_objects_with_size(obj_list)
+            ctx.exit()
+
+        obj = obj_list[0]
+        filepath, err = cf_r2.get_download_filepath(obj['key'], folder, dest)
+        if err.find("文件夹不存在") >= 0:
+            print_err(err)
+            ctx.exit()
+
+        if err.find("文件已存在") >= 0:
+            print(err)
+            click.confirm("要下载并覆盖文件吗?", abort=True)
+            err = ""
+
+        if not err:
+            cf_r2.download_file(the_bucket, obj['key'], obj['size'], filepath)
 
 
 if __name__ == "__main__":
